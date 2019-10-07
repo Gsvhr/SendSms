89cbbcd53e0b54d107a9e7760a2d4974420eacd8
step <79278993666@ya.ru>
2019-10-07 14:15:08 +0400

    Add Template Page

--- SendSms/ViewModels/MainViewModel.cs
+++ SendSms/ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using SendSms.Core.Models;
 using SendSms.Core.Services;
 using SendSms.Helpers;
@@ -17,6 +18,36 @@ namespace SendSms.ViewModels
             set { Set(ref _balance, value); }
         }
 
+        private string _phone;
+        public string Phone 
+        {
+            get { return _phone; }
+
+            set { Set(ref _phone, value); }
+        }
+
+        private string _text;
+        public string Text 
+        {
+            get { return _text; }
+
+            set { Set(ref _text, value); }
+        }
+
+        private ICommand _sendCommand;
+        public ICommand SendCommand 
+        {
+            get
+            {
+                if (_sendCommand == null)
+                {
+                    _sendCommand = new RelayCommand(SendSms);
+                }
+
+                return _sendCommand;
+            }
+        }
+
         public MainViewModel()
         {
         }
@@ -25,6 +56,16 @@ namespace SendSms.ViewModels
         {
             var b = await http.GetAsync<AccountBalance>("my/balance?api_id=12E2FE13-B040-F7FA-D57C-CF42531E9DFE&json=1");
             Balance = "Баланс составляет: " + b.Balance.ToString() + " руб.";
+        }
+        public async Task LoadPriceAsync() 
+        {
+            var b = await http.GetAsync<AccountBalance>("my/balance?api_id=12E2FE13-B040-F7FA-D57C-CF42531E9DFE&json=1");
+            Balance = "Баланс составляет: " + b.Balance.ToString() + " руб.";
+        }
+
+        public void SendSms ()
+        {
+            var b = 7;
         }
     }
 }
