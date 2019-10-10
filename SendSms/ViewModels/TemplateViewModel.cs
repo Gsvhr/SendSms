05c3eacc946b7b52193505497f61f99bea769b5e
Stepan Gorshenin <gsv.tlt@gmail.com>
2019-10-11 00:11:34 +0400

    Add Templates

--- SendSms/ViewModels/TemplateViewModel.cs
+++ SendSms/ViewModels/TemplateViewModel.cs
@@ -1,12 +1,64 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using SendSms.Core.Models;
+using SendSms.EntityFramework;
 using SendSms.Helpers;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SendSms.ViewModels
 {
     public class TemplateViewModel : Observable
     {
+        public ObservableCollection<Template> Source { get; } = new ObservableCollection<Template>();
+
+        private Template _selected;
+        public Template Selected
+        {
+            get => _selected;
+            set => Set(ref _selected, value);
+        }
+
+        private ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get
+            {
+                if (_addCommand == null)
+                {
+                    _addCommand = new RelayCommand(AddTemplate);
+                }
+
+                return _addCommand;
+            }
+        }
+
+        private void AddTemplate()
+        {
+            var t = new Template();
+            Source.Add(t);
+            Selected = t;
+        }
+
         public TemplateViewModel()
         {
+        }
+
+        public async Task LoadDataAsync()
+        {
+            Source.Clear();
+
+            using (var db = new SendSmsContext())
+            {
+                var data = await db.Templates.AsNoTracking().OrderByDescending(x => x.Title).ToListAsync();
+                foreach (var item in data)
+                {
+                    Source.Add(item);
+                }
+            }
         }
     }
 }
--- SendSms/Views/TemplatePage.xaml.cs
+++ SendSms/Views/TemplatePage.xaml.cs
@@ -1,7 +1,9 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using SendSms.EntityFramework;
 using SendSms.ViewModels;
-
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace SendSms.Views
 {
@@ -12,6 +14,32 @@ namespace SendSms.Views
         public TemplatePage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            await ViewModel.LoadDataAsync();
+        }
+
+        private void DataGrid_CellEditEnded(object sender, Microsoft.Toolkit.Uwp.UI.Controls.DataGridCellEditEndedEventArgs e)
+        {
+            var t = ViewModel.Selected;
+
+            using (var db = new SendSmsContext())
+            {
+                var n = db.Templates.FirstOrDefault(x => x.Id == t.Id);
+                if (n == null)
+                {
+                    db.Add(t);
+                }
+                else
+                {
+                    db.Entry(n).CurrentValues.SetValues(t);
+                }
+                db.SaveChanges();
+            }
         }
     }
 }
